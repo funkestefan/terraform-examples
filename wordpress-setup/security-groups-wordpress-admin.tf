1ca6323d9c8e6cb699c714c2b2ae747c406f1c54
Stefan Funke <s.funke@syseleven.de>
2016-09-27 10:51:54 +0200

    admin host init

---
+++ wordpress-setup/security-groups-wordpress-admin.tf
@@ -0,0 +1,47 @@
+## admin host Security Groups
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_admin_1" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol = "icmp"
+  remote_ip_prefix = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_wordpress.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_admin_2" {
+  direction = "egress"
+  ethertype = "IPv4"
+  protocol = "icmp"
+  remote_ip_prefix = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_wordpress.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_admin_3" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol = "tcp"
+  port_range_min = 22
+  port_range_max = 22
+  remote_ip_prefix = "${var.sshallow}"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_wordpress.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_admin_4" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol = "tcp"
+  port_range_min = 22
+  port_range_max = 22
+  remote_ip_prefix = "${var.sshallow}"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_wordpress.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_admin_5" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol = "tcp"
+  port_range_min = 22
+  port_range_max = 22
+  remote_ip_prefix = "${var.homenet}"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_wordpress.id}"
+}

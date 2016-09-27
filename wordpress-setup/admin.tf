b367d18f899decdc366ee1309cad5ce5d502d220
Stefan Funke <s.funke@syseleven.de>
2016-09-27 11:40:33 +0200

    error in sg assignment for admin host

--- wordpress-setup/admin.tf
+++ wordpress-setup/admin.tf
@@ -3,7 +3,7 @@ resource "openstack_compute_instance_v2" "terraform-admin" {
   image_name = "${var.image}"
   flavor_name = "${var.flavormicro}"
   user_data = "${file("cloud-init/wordpress-admin.yaml")}"
-  security_groups = [ "${openstack_networking_secgroup_v2.secgroup_wordpress.id}" ]
+  security_groups = [ "${openstack_networking_secgroup_v2.secgroup_wordpress_admin.id}" ]
   floating_ip = "${openstack_compute_floatingip_v2.adminip.address}"
   network {
     uuid = "${openstack_networking_network_v2.terraform.id}"

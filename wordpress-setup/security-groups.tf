c13b0fea0293f16022872465ca911a47c04bf4e4
Stefan Funke <s.funke@syseleven.de>
2016-09-28 15:46:45 +0200

    added app sg

--- wordpress-setup/security-groups.tf
+++ wordpress-setup/security-groups.tf
@@ -14,3 +14,8 @@ resource "openstack_networking_secgroup_v2" "secgroup_wordpress_admin" {
   name = "terra-secgroup_wordpress_admin"
   description = "Security Group for wordpress database node"
 }
+
+resource "openstack_networking_secgroup_v2" "secgroup_wordpress_app" {
+  name = "terra-secgroup_wordpress_app"
+  description = "Security Group for wordpress database node"
+}
